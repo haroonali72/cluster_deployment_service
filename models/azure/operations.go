1834670ceee25c0c390050ce5372344c9a68d166
sadaf <sadaf@cloudplex.io>
2019-01-29 17:15:25 +0500

    azure sdk added

--- models/azure/operations.go
+++ models/azure/operations.go
@@ -1,1 +1,10 @@
 package azure
+import (
+"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-04-01/compute"
+"github.com/Azure/azure-sdk-for-go/services/marketplaceordering/mgmt/2015-06-01/marketplaceordering"
+"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-02-01/network"
+storage "github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2017-10-01/storage"
+storageblob "github.com/Azure/azure-sdk-for-go/storage"
+"github.com/Azure/go-autorest/autorest"
+"github.com/Azure/go-autorest/autorest/azure"
+"github.com/Azure/go-autorest/autorest/to" )
