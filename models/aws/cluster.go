ac9d4ee1df0bbd12cc354c362706120ffd1b015f
sadaf <sadaf@cloudplex.io>
2019-02-27 13:15:54 +0500

    status key changed

--- models/aws/cluster.go
+++ models/aws/cluster.go
@@ -190,7 +190,7 @@ func DeployCluster(cluster Cluster_Def, credentials string) error {
 		logging.SendLog("Cluster creation failed : "+cluster.Name, "error", cluster.ProjectId)
 		logging.SendLog(err.Error(), "error", cluster.ProjectId)
 
-		cluster.Status = "Cluster creation failed"
+		cluster.Status = "Cluster Creation Failed"
 
 		err = UpdateCluster(cluster)
 		if err != nil {
@@ -288,7 +288,7 @@ func TerminateCluster(cluster Cluster_Def, credentials string) error {
 		logging.SendLog("Cluster termination failed: "+cluster.Name, "error", cluster.ProjectId)
 		logging.SendLog(err.Error(), "error", cluster.ProjectId)
 
-		cluster.Status = "Cluster termination failed"
+		cluster.Status = "Cluster Termination Failed"
 		err = UpdateCluster(cluster)
 		if err != nil {
 			beego.Error("Cluster model: Deploy - Got error while connecting to the database: ", err.Error())
@@ -305,7 +305,7 @@ func TerminateCluster(cluster Cluster_Def, credentials string) error {
 		var nodes []*Node
 		pools.Nodes = nodes
 	}
-	cluster.Status = "Cluster terminated"
+	cluster.Status = "Cluster Terminated"
 	err = UpdateCluster(cluster)
 	if err != nil {
 		beego.Error("Cluster model: Deploy - Got error while connecting to the database: ", err.Error())
