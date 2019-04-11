41756e0f2d3b5368b2b35027bbfd3887290f3538
sadaf <sadaf@cloudplex.io>
2019-04-11 15:52:57 +0500

    amis api fix for swagger

--- controllers/aws/cluster.go
+++ controllers/aws/cluster.go
@@ -341,51 +341,14 @@ func (c *AWSClusterController) GetSSHKeys() {
 	c.ServeJSON()
 }
 
-// @Title AwsSshKeyPair
-// @Description returns aws ssh key pairs
+// @Title AwsAmis
+// @Description returns aws ami details
 // @Param	Authorization	header	string	false	"{access_key}:{secret_key}:{region}"
-// @Success 200 {object} []*ec2.KeyPairInfo
+// @Param	amiId	path	string	true	"Id of the ami"
+// @Success 200 {object} []*ec2.BlockDeviceMapping
 // @Failure 401 {"error": "exception_message"}
 // @Failure 500 {"error": "internal server error"}
-// @router /awssshkeys [get]
-func (c *AWSClusterController) GetSSHKeyPairs() {
-
-	beego.Info("AWSNetworkController: FetchExistingVpcs.")
-	credentials := c.Ctx.Input.Header("Authorization")
-
-	if credentials == "" ||
-		strings.Contains(credentials, " ") ||
-		strings.Contains(strings.ToLower(credentials), "bearer") ||
-		strings.Contains(strings.ToLower(credentials), "aws") ||
-		len(strings.Split(credentials, ":")) != 3 {
-		c.Ctx.Output.SetStatus(401)
-		c.Data["json"] = map[string]string{"error": "Authorization format should be '{access_key}:{secret_key}:{region}'"}
-		c.ServeJSON()
-		return
-	}
-
-	beego.Info("AWSClusterController: Get Keys from AWS")
-
-	keys, err := aws.GetAwsSSHKeyPair(credentials)
-
-	if err != nil {
-		c.Ctx.Output.SetStatus(500)
-		c.Data["json"] = map[string]string{"error": "internal server error"}
-		c.ServeJSON()
-		return
-	}
-
-	c.Data["json"] = keys
-	c.ServeJSON()
-}
-
-// @Title AWSAmis
-// @Description returns aws ami detials
-// @Param	Authorization	header	string	false	"{access_key}:{secret_key}:{region}"
-// @Success 200 {object} []*ec2.KeyPairInfo
-// @Failure 401 {"error": "exception_message"}
-// @Failure 500 {"error": "internal server error"}
-// @router /amis/:amiId/ [ [get]
+// @router /:amiId [get]
 func (c *AWSClusterController) GetAMI() {
 
 	beego.Info("AWSNetworkController: FetchExistingVpcs.")
