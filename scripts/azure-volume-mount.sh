44361e03d2f90493c27d0b345210f2833d0ea874
sadaf <sadaf@cloudplex.io>
2019-04-16 14:17:52 +0500

    azure mount script added

---
+++ scripts/azure-volume-mount.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+  sudo /sbin/wipefs /dev/sdc
+  sudo /sbin/mkfs.ext4 /dev/sdc
+  sudo mkdir /data0
+  sudo mount /dev/sdc /data0
+  sudo /bin/chmod 777 /dev/sdc
+  sudo su <<-EOF
+  sudo foo=blkid | awk '{print $2}' | sed -n '3p'
+  echo '${foo} /data0 ext4 defaults,nofail 1 2'
+    EOF
