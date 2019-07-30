73039c17e194474b80c22d2433bd5c2c7d309a0b
sadaf <sadaf@cloudplex.io>
2019-07-30 13:47:11 +0500

    mongo port correction

--- models/utils/commandline_agrs.go
+++ models/utils/commandline_agrs.go
@@ -142,7 +142,7 @@ func InitFlags() error {
 		return err
 	}
 
-	mongo = mongo + ":32180"
+	mongo = mongo + ":27017"
 	redis_url = redis_url + ":6379"
 	network_url = "http://" + network_url + ":9080/weasel/network/{cloud_provider}"
 	vault_url = "http://" + vault_url + ":8092/robin/api/v1"
