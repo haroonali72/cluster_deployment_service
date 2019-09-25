c67b1d32a8abbbcc9c0b9fad61dcb1d6d6ebb4aa
zunaira518 <zunaira518@gmail.com>
2019-09-25 18:08:17 +0500

    library added

---
+++ routers/commentsRouter____home_zunaira_cloudplex_repository_cloud_native_platform_src_antelope_controllers.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["antelope/controllers:HealthController"] = append(beego.GlobalControllerRouter["antelope/controllers:HealthController"],
+        beego.ControllerComments{
+            Method: "Get",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
