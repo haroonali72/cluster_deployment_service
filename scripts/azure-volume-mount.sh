486417af7559f7f2845f772cdc47e9fa36c8cdd0
zunaira1231 <zunaira.siddique@cloudplex.io>
2019-12-05 17:02:30 +0500

    data2 replaced with data1

--- scripts/azure-volume-mount.sh
+++ scripts/azure-volume-mount.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
   sudo /sbin/wipefs /dev/sdd
   sudo /sbin/mkfs.ext4 /dev/sdd
-  sudo mkdir /data2
-  sudo mount /dev/sdd /data2
+  sudo mkdir /data1
+  sudo mount /dev/sdd /data1
   sudo /bin/chmod 777 /dev/sdd
   sudo su <<-EOF
   sudo foo=blkid | awk '{print $2}' | sed -n '3p'
-  echo '${foo} /data2 ext4 defaults,nofail 1 2'
+  echo '${foo} /data1 ext4 defaults,nofail 1 2'
     EOF
