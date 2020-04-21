f625ecaa05c9d351d1ccda19de37d57c1c332bac
Zunaira Siddique <zunaira518@gmail.com>
2020-04-21 18:19:16 +0500

    iks error messages and status code handled

--- routers/commentsRouter_controllers_op.go
+++ routers/commentsRouter_controllers_op.go
@@ -7,94 +7,94 @@ import (
 
 func init() {
 
-	beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"],
-		beego.ControllerComments{
-			Method:           "Post",
-			Router:           `/`,
-			AllowHTTPMethods: []string{"post"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"],
-		beego.ControllerComments{
-			Method:           "Patch",
-			Router:           `/`,
-			AllowHTTPMethods: []string{"put"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"],
+        beego.ControllerComments{
+            Method: "Patch",
+            Router: `/`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"],
-		beego.ControllerComments{
-			Method:           "Get",
-			Router:           `/:projectId/`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"],
+        beego.ControllerComments{
+            Method: "Get",
+            Router: `/:projectId/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"],
-		beego.ControllerComments{
-			Method:           "Delete",
-			Router:           `/:projectId/:forceDelete`,
-			AllowHTTPMethods: []string{"delete"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:projectId/:forceDelete`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"],
-		beego.ControllerComments{
-			Method:           "GetAll",
-			Router:           `/all`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPClusterController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/all`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"],
-		beego.ControllerComments{
-			Method:           "Post",
-			Router:           `/`,
-			AllowHTTPMethods: []string{"post"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"],
-		beego.ControllerComments{
-			Method:           "Patch",
-			Router:           `/`,
-			AllowHTTPMethods: []string{"put"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"],
+        beego.ControllerComments{
+            Method: "Patch",
+            Router: `/`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"],
-		beego.ControllerComments{
-			Method:           "Delete",
-			Router:           `/:templateId`,
-			AllowHTTPMethods: []string{"delete"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:templateId`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"],
-		beego.ControllerComments{
-			Method:           "Get",
-			Router:           `/:templateId/`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"],
+        beego.ControllerComments{
+            Method: "Get",
+            Router: `/:templateId/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
-	beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"],
-		beego.ControllerComments{
-			Method:           "GetAll",
-			Router:           `/all`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
+    beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"] = append(beego.GlobalControllerRouter["antelope/controllers/op:OPTemplateController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/all`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
 
 }
