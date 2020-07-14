3752dafb42e121763670d3d13d94d26408021d15
sadaf <sadaf@cloudplex.io>
2020-07-14 22:20:24 +0500

    EKS fixes

--- models/eks/cluster.go
+++ models/eks/cluster.go
@@ -800,7 +800,9 @@ func PatchRunningEKSCluster(cluster EKSCluster, credentials vault.AwsCredentials
 	}
 	loggingChanges := false
 	for _, dif := range difCluster {
-
+		if dif.Type != "update" {
+			continue
+		}
 		if len(dif.Path) > 2 {
 			poolIndex, _ := strconv.Atoi(dif.Path[1])
 			if poolIndex > (previousPoolCount - 1) {
